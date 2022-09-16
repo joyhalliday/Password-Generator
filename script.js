cf2baf79496222b618d73dd67f2301b71ba5c5e3
Joy Halliday <tjoyhalliday@gmail.com>
2022-09-15 21:54:52 -0700

    I am trying

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
+alert("ahhhh")
+
 // Assignment Code
-const generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
+const lowerCase = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+const upperCase = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
+//const specialChara = [!, #, %, &, *, +, @]
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
-  const password = generatePassword();
-  const passwordText = document.querySelector("#password");
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); 
+
+
+//if (lowerCase) {
+ // alert("Your password will include lower case letters.")
+//} else {
+  //alert("Your password will not include lower case letters.")
+//}
